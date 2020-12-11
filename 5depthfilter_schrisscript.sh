1dc753c6e0bde44aaf1aa316b666840c72c8bfbd
kristenadaley <66744262+kristenadaley@users.noreply.github.com>
2020-12-11 14:29:05 -0800

    Create 5depthfilter_schrisscript.sh

---
+++ 5depthfilter_schrisscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mafFiles=$(ls /projects/rmorin_scratch/BC_ctDNA_cohort/4filtered_strelkaMAFs/*.maf)
+outDir="/projects/rmorin_scratch/BC_ctDNA_cohort/5depthfiltered_strelkaMAFs/"
+
+for mafFile in ${mafFiles[*]}; do
+
+        baseName=${mafFile##*/}
+        rootName=${baseName%%.*}
+        outName=${outDir}/${rootName}.depthFilt.maf
+
+        awk -F '\t' '$40>=10' $mafFile > $outName
+done
